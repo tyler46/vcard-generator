bdaeec51610556642617cce5b13a0ad9c2cd41d8
tyler46 <mail.doctor46@gmail.com>
2015-03-25 11:55:46 +0200

    Remove old files.

--- src/test.js
+++
@@ -1,5 +0,0 @@
-var card = require('./card');
-
-var contact = {name: {firstName: 'John', lastName: 'Smith', prefix: 'Mr'}, nickname: 'Little John'};
-
-console.log(card(contact));
--- src/vcardgen.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-module.exports = function (args) {
-  return 'New vCard';
-};
-
