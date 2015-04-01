a79cae5dbe7008139e168079abd7c99787807522
tyler46 <mail.doctor46@gmail.com>
2015-04-02 00:53:25 +0300

    Fix error when mutliple emails.

--- src/filters/email.js
+++ src/filters/email.js
@@ -8,8 +8,8 @@ exports.visit = function (obj) {
     emails = obj.email.map(function (e) {
       var mail = {};
       mail.address = (!_.isUndefined(e.address)) ? ':' + e.address : '';
-      mail.type = (!_.isUndefined(e.type)) ? 'type=' + e.type.toUpperCase() + ';' : '';
-      mail.primary = (!_.isUndefined(e.primary)) ? 'type=pref' : '';
+      mail.type = (!_.isUndefined(e.type)) ? 'type=' + e.type.toUpperCase() : '';
+      mail.primary = (!_.isUndefined(e.primary)) ? ';type=pref' : '';
 
       return 'EMAIL;type=INTERNET;' + mail.type + mail.primary + mail.address;
     });
